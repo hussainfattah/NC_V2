f5edcf38fb02d7697c3deb0a579fb4839409bcd9
H M Abdul Fattah <111952077+hmfattah@users.noreply.github.com>
2023-10-17 16:22:25 -0700

    Update sample_test.py

--- implementation/sample_test.py
+++ implementation/sample_test.py
@@ -1,11 +1,40 @@
 import os 
+import git
+
+# Optionally, you can also set the branch to track the remote branch
+# repo.heads[branch_name].set_tracking_branch(repo.remotes.origin[branch_name])
+
+# Now the changes are pushed to the remote repository
 
 with open('demo.txt', 'w') as file:
   size = 10000
   print('demo output size: ', size, file = file)
 
-os.system("git config --global user.name hmfattah")
-os.system("git config --global user.email hmfattah@arizona.edu")
-os.system("git add .")
-os.system("git commit -m message")
-os.system("git push")
+# Define the path to your Git repository
+repo_path = '/negation_for_common_sense'
+
+#https://github.com/hmfattah/negation_for_common_sense
+
+# Initialize a Git repository object
+repo = git.Repo(repo_path)
+
+# Define the branch you want to push to (e.g., 'main' or 'master')
+branch_name = 'main'
+
+# Fetch the remote repository to ensure you have the latest changes
+repo.remotes.origin.fetch()
+
+# Switch to the branch you want to push
+repo.git.checkout(branch_name)
+
+# Make some changes to your files (e.g., modifying a file)
+
+# Add the changes to the staging area
+repo.index.add(['demo.txt'])
+
+# Commit the changes
+commit_message = "Your commit message here"
+repo.index.commit(commit_message)
+
+# Push the changes to the remote repository
+repo.remotes.origin.push(branch_name)
