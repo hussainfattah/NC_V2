faed10f6917478845ff863648cd4abfb9149ce86
H M Abdul Fattah <111952077+hmfattah@users.noreply.github.com>
2023-10-17 16:13:41 -0700

    git

--- implementation/sample_test.py
+++ implementation/sample_test.py
@@ -4,6 +4,8 @@ with open('demo.txt', 'w') as file:
   size = 10000
   print('demo output size: ', size, file = file)
 
+os.system("git config --global user.name hmfattah")
+os.system("git config --global user.email hmfattah@arizona.edu")
 os.system("git add .")
 os.system("git commit -m message")
 os.system("git push")
